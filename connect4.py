0036b6418ee823f9d5b257e230e6a6d24493ea23
Jolin520 <tun28156@temple.edu>
2022-11-04 09:56:40 +0300

    edited connect and button files

--- connect4.py
+++ connect4.py
@@ -1,6 +1,9 @@
 import numpy as np
+import pygame
 import sys
+import math
 from Button import *
+import random
 
 pygame.init()
 
@@ -11,131 +14,357 @@ BLUE = (0, 0, 255)
 LIGHT_BLUE = (0, 100, 255)
 LIGHT_WHITE = (170, 170, 170)
 DARK_WHITE = (100, 100, 100)
+RED = (255,0,0)
+YELLOW = (255,255,0)
 
 #Screen
-width = 1280
+width = 720
 height = 720
 res = (width,height)
 screen = pygame.display.set_mode(res)
 
-BG = pygame.image.load("Dots.jpeg")
+ROW_COUNT = 6
+COLUMN_COUNT = 7
+
+PLAYER = 0
+COMPUTER = 1
+
+EMPTY = 0
+PLAYER_PIECE = 1
+COMPUTER_PIECE = 2
+
+WINDOW_LENGTH = 4
+
+BG = pygame.image.load("Dots_BG.jpeg")
 BG = pygame.transform.scale(BG, res)
 
-#chips ratio to screen
+# chips ratio to screen
 if width > height:
     RADIUS = int(height/15)
 else:
     RADIUS = int(width/15)
 
-rows = 6
-cols = 7
+myfont = pygame.font.SysFont("monospace", 75)
 
-def board_gen_gui(screen):
-    for c in range(cols):
-        for r in range(rows):
-            pygame.draw.circle(screen, WHITE, (int((c * height/rows) + 1.5*RADIUS), int((r * height/rows) + 1.5*RADIUS)),
-                                   RADIUS)
-
-    pygame.display.update()
-
-def board_gen():
-    board=np.zeros((rows,cols))
+def create_board():
+    board = np.zeros((ROW_COUNT,COLUMN_COUNT))
     return board
 
-def main_menu():
-    while True:
-        for choice in pygame.event.get():
-            if choice.type == pygame.QUIT:
-                pygame.quit()
+def drop_piece(board, row, col, piece):
+    board[row][col] = piece
 
-            if choice.type == pygame.MOUSEBUTTONDOWN:
-                if (button_x - (button_width / 2)) <= mouse[0] <= (button_x + (button_width / 2)) \
-                        and (single_y - (button_height / 2)) <= mouse[1] <= (single_y + (button_height / 2)):
-                    single()
-                elif (button_x - (button_width/2)) <= mouse[0] <= (button_x + (button_width/2)) \
-                             and (multi_y - (button_height/2)) <= mouse[1] <= (multi_y + (button_height/2)):
-                    multi()
-                elif (button_x - (button_width/2)) <= mouse[0] <= (button_x + (button_width/2)) \
-                             and (rules_y - (button_height/2)) <= mouse[1] <= (rules_y + (button_height/2)):
-                    rules()
-                elif (button_x - (quit_width/2)) <= mouse[0] <= (button_x + (quit_width/2)) \
-                             and (quit_y - (quit_height/2)) <= mouse[1] <= (quit_y + (quit_height/2)):
-                    pygame.quit()
+def is_valid_location(board, col):
+    return board[ROW_COUNT-1][col] == 0
 
-        screen.blit(BG, (0, 0))
-        mouse = pygame.mouse.get_pos()
+def get_next_open_row(board, col):
+    for r in range(ROW_COUNT):
+        if board[r][col] == 0:
+            return r
 
-        #Button size
-        button_width = width/5
-        button_height = height/15
-        button_x = width/2
+def print_board(board):
+    print(np.flip(board, 0))
 
-        #buttons
-        #single player button
-        single_y = height/2.7
+def winning_move(board, piece):
+    # Check horizontal locations for win
+    for c in range(COLUMN_COUNT-3):
+        for r in range(ROW_COUNT):
+            if board[r][c] == piece and board[r][c+1] == piece and board[r][c+2] == piece and board[r][c+3] == piece:
+                return True
+ 
+    # Check vertical locations for win
+    for c in range(COLUMN_COUNT):
+        for r in range(ROW_COUNT-3):
+            if board[r][c] == piece and board[r+1][c] == piece and board[r+2][c] == piece and board[r+3][c] == piece:
+                return True
+ 
+    # Check positively sloped diaganols
+    for c in range(COLUMN_COUNT-3):
+        for r in range(ROW_COUNT-3):
+            if board[r][c] == piece and board[r+1][c+1] == piece and board[r+2][c+2] == piece and board[r+3][c+3] == piece:
+                return True
+ 
+    # Check negatively sloped diaganols
+    for c in range(COLUMN_COUNT-3):
+        for r in range(3, ROW_COUNT):
+            if board[r][c] == piece and board[r-1][c+1] == piece and board[r-2][c+2] == piece and board[r-3][c+3] == piece:
+                return True
 
-        single_button = Button(button_x, single_y, button_width, button_height)
+def draw_board(board):
+    for c in range(COLUMN_COUNT):
+        for r in range(ROW_COUNT):
+            pygame.draw.rect(screen, LIGHT_BLUE, (c*SQUARESIZE, r*SQUARESIZE+SQUARESIZE, SQUARESIZE, SQUARESIZE))
+            pygame.draw.circle(screen, BLACK, (int(c*SQUARESIZE+SQUARESIZE/2), int(r*SQUARESIZE+SQUARESIZE+SQUARESIZE/2)), RADIUS)
+     
+    for c in range(COLUMN_COUNT):
+        for r in range(ROW_COUNT):      
+            if board[r][c] == PLAYER_PIECE:
+                pygame.draw.circle(screen, RED, (int(c*SQUARESIZE+SQUARESIZE/2), height-int(r*SQUARESIZE+SQUARESIZE/2)), RADIUS)
+            elif board[r][c] == COMPUTER_PIECE: 
+                pygame.draw.circle(screen, YELLOW, (int(c*SQUARESIZE+SQUARESIZE/2), height-int(r*SQUARESIZE+SQUARESIZE/2)), RADIUS)
+    pygame.display.update()
 
-        if (button_x - (button_width/2)) <= mouse[0] <= (button_x + (button_width/2)) \
-                and (single_y - (button_height/2)) <= mouse[1] <= (single_y + (button_height/2)):
-            single_button.draw(screen, DARK_WHITE, 0, WHITE, 'One-Player')
-        single_button.draw(screen, WHITE, 1, WHITE, 'One-Player')
+#define our screen size
+SQUARESIZE = 100
+ 
+#define width and height of board
+width = COLUMN_COUNT * SQUARESIZE
+height = (ROW_COUNT+1) * SQUARESIZE
 
-        #two player button
-        multi_y = height / 2.2
+def evaluate_window(window, piece):
+	score = 0
+	opp_piece = PLAYER_PIECE
+	if piece == PLAYER_PIECE:
+		opp_piece = COMPUTER_PIECE
 
-        multi_button = Button(button_x, multi_y, button_width, button_height)
+	if window.count(piece) == 4:
+		score += 100
+	elif window.count(piece) == 3 and window.count(EMPTY) == 1:
+		score += 5
+	elif window.count(piece) == 2 and window.count(EMPTY) == 2:
+		score += 2
 
-        if (button_x - (button_width/2)) <= mouse[0] <= (button_x + (button_width/2)) \
-                and (multi_y - (button_height/2)) <= mouse[1] <= (multi_y + (button_height/2)):
-            multi_button.draw(screen, DARK_WHITE, 0, WHITE, 'Two-Player')
-        multi_button.draw(screen, WHITE, 1, WHITE, 'Two-Player')
+	if window.count(opp_piece) == 3 and window.count(EMPTY) == 1:
+		score -= 4
 
-        #rules button
-        rules_y = height/1.85
+	return score
 
-        rules_button = Button(button_x, rules_y, button_width, button_height)
+def score_position(board, piece):
+	score = 0
 
-        if (button_x - (button_width/2)) <= mouse[0] <= (button_x + (button_width/2)) \
-                and (rules_y - (button_height/2)) <= mouse[1] <= (rules_y + (button_height/2)):
-            rules_button.draw(screen, DARK_WHITE, 0, WHITE, 'Rules')
-        rules_button.draw(screen, WHITE, 1, WHITE, 'Rules')
+	## Score center column
+	center_array = [int(i) for i in list(board[:, COLUMN_COUNT//2])]
+	center_count = center_array.count(piece)
+	score += center_count * 3
 
-        #quit button
-        quit_y = height/1.55
-        quit_width = width/15
-        quit_height = height/18
+	## Score Horizontal
+	for r in range(ROW_COUNT):
+		row_array = [int(i) for i in list(board[r,:])]
+		for c in range(COLUMN_COUNT-3):
+			window = row_array[c:c+WINDOW_LENGTH]
+			score += evaluate_window(window, piece)
 
-        quit_button = Button(button_x, quit_y, quit_width, quit_height)
+	## Score Vertical
+	for c in range(COLUMN_COUNT):
+		col_array = [int(i) for i in list(board[:,c])]
+		for r in range(ROW_COUNT-3):
+			window = col_array[r:r+WINDOW_LENGTH]
+			score += evaluate_window(window, piece)
 
-        quit_button.draw(screen, LIGHT_WHITE, 0, BLACK, 'Quit')
-        if (button_x - (quit_width/2)) <= mouse[0] <= (button_x + (quit_width/2)) \
-                and (quit_y - (quit_height/2)) <= mouse[1] <= (quit_y + (quit_height/2)):
-            quit_button.draw(screen, DARK_WHITE, 0, WHITE, 'Quit')
+	## Score posiive sloped diagonal
+	for r in range(ROW_COUNT-3):
+		for c in range(COLUMN_COUNT-3):
+			window = [board[r+i][c+i] for i in range(WINDOW_LENGTH)]
+			score += evaluate_window(window, piece)
 
+	for r in range(ROW_COUNT-3):
+		for c in range(COLUMN_COUNT-3):
+			window = [board[r+3-i][c+i] for i in range(WINDOW_LENGTH)]
+			score += evaluate_window(window, piece)
+
+	return score
+
+def pick_best_move(board, piece):
+
+	valid_locations = get_valid_locations(board)
+	best_score = -10000
+	best_col = random.choice(valid_locations)
+	for col in valid_locations:
+		row = get_next_open_row(board, col)
+		temp_board = board.copy()
+		drop_piece(temp_board, row, col, piece)
+		score = score_position(temp_board, piece)
+		if score > best_score:
+			best_score = score
+			best_col = col
+
+	return best_col
+
+def get_valid_locations(board):
+	valid_locations = []
+	for col in range(COLUMN_COUNT):
+		if is_valid_location(board, col):
+			valid_locations.append(col)
+	return valid_locations
+
+def is_terminal_node(board):
+	return winning_move(board, PLAYER_PIECE) or winning_move(board, COMPUTER_PIECE) or len(get_valid_locations(board)) == 0
+
+
+def minimax(board, depth, alpha, beta, maximizingPlayer):
+	valid_locations = get_valid_locations(board)
+	is_terminal = is_terminal_node(board)
+	if depth == 0 or is_terminal:
+		if is_terminal:
+			if winning_move(board, COMPUTER_PIECE):
+				return (None, 100000000000000)
+			elif winning_move(board, PLAYER_PIECE):
+				return (None, -10000000000000)
+			else: # Game is over, no more valid moves
+				return (None, 0)
+		else: # Depth is zero
+			return (None, score_position(board, COMPUTER_PIECE))
+	if maximizingPlayer:
+		value = -math.inf
+		column = random.choice(valid_locations)
+		for col in valid_locations:
+			row = get_next_open_row(board, col)
+			b_copy = board.copy()
+			drop_piece(b_copy, row, col, COMPUTER_PIECE)
+			new_score = minimax(b_copy, depth-1, alpha, beta, False)[1]
+			if new_score > value:
+				value = new_score
+				column = col
+			alpha = max(alpha, value)
+			if alpha >= beta:
+				break
+		return column, value
+
+	else: # Minimizing player
+		value = math.inf
+		column = random.choice(valid_locations)
+		for col in valid_locations:
+			row = get_next_open_row(board, col)
+			b_copy = board.copy()
+			drop_piece(b_copy, row, col, PLAYER_PIECE)
+			new_score = minimax(b_copy, depth-1, alpha, beta, True)[1]
+			if new_score < value:
+				value = new_score
+				column = col
+			beta = min(beta, value)
+			if alpha >= beta:
+				break
+		return column, value
+
+
+def single():
+    board = create_board()
+
+    draw_board(board)
+    pygame.display.update()
+
+    game_over = False
+    turn = random.randint(PLAYER, COMPUTER)
+
+    while not game_over:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
 
-        pygame.display.update()
+            if event.type == pygame.MOUSEMOTION:
+                pygame.draw.rect(screen, LIGHT_BLUE, (0, 0, width, SQUARESIZE))
+                posx = event.pos[0]
+                if turn == PLAYER:
+                    pygame.draw.circle(screen, RED, (posx, int(SQUARESIZE/2)), RADIUS)
 
-def single():
-    while True:
-        screen.fill(LIGHT_BLUE)
+            pygame.display.update()
 
-        board = board_gen()
-        board_gen_gui(screen)
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                pygame.draw.rect(screen, LIGHT_BLUE, (0, 0, width, SQUARESIZE))
+                #print(event.pos)
+                if turn == PLAYER:
+                    posx = event.pos[0]
+                    col = int(math.floor(posx/SQUARESIZE))
+                    if is_valid_location(board, col):
+                        row = get_next_open_row(board, col)
+                        drop_piece(board, row, col, PLAYER_PIECE)
 
-        pygame.display.update()
+                        if winning_move(board, PLAYER_PIECE):
+                            label = myfont.render("Player 1 wins!!", 1, RED)
+                            screen.blit(label, (40, 10))
+                            game_over = True
 
+                        turn += 1
+                        turn = turn % 2
+
+                        print_board(board)
+                        draw_board(board)
+
+                # Ask for Player 2 Input
+        if turn == COMPUTER and not game_over:
+            col, minimax_score = minimax(board, 5, -math.inf, math.inf, True)
+
+            if is_valid_location(board, col):
+                pygame.time.wait(500)
+                row = get_next_open_row(board, col)
+                drop_piece(board, row, col, COMPUTER_PIECE)
+
+                if winning_move(board, COMPUTER_PIECE):
+                    label = myfont.render("Computer wins!!", 1, YELLOW)
+                    screen.blit(label, (40, 10))
+                    game_over = True
+
+                print_board(board)
+                draw_board(board)
+
+                turn += 1
+                turn = turn % 2
+
+        if game_over:
+            pygame.time.wait(3000)
+
+
+               
 def multi():
-    while True:
-        screen.fill(BLUE)
+    board = create_board()
+    
+    draw_board(board)
+    pygame.display.update()
 
-        board = board_gen()
-        board_gen_gui(screen)
+    game_over = False
+    turn = 0
 
-        pygame.display.update()
+    while not game_over:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+            if event.type == pygame.MOUSEMOTION:
+                pygame.draw.rect(screen, LIGHT_BLUE, (0, 0, width, SQUARESIZE))
+                posx = event.pos[0]
+                if turn == 0:
+                    pygame.draw.circle(screen, RED, (posx, int(SQUARESIZE/2)), RADIUS)
+                else:
+                    pygame.draw.circle(screen, YELLOW, (posx, int(SQUARESIZE/2)), RADIUS)
+
+            pygame.display.update()
+
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                pygame.draw.rect(screen, LIGHT_BLUE, (0, 0, width, SQUARESIZE))
+                #print(event.pos)
+                if turn == 0:
+                    posx = event.pos[0]
+                    col = int(math.floor(posx/SQUARESIZE))
+                    if is_valid_location(board, col):
+                        row = get_next_open_row(board, col)
+                        drop_piece(board, row, col, 1)
+
+                        if winning_move(board, 1):
+                            label = myfont.render("Player 1 wins!!", 1, RED)
+                            screen.blit(label, (40, 10))
+                            game_over = True
+
+                # Ask for Player 2 Input
+                else:
+                    posx = event.pos[0]
+                    col = int(math.floor(posx / SQUARESIZE))
+                    if is_valid_location(board, col):
+                        row = get_next_open_row(board, col)
+                        drop_piece(board, row, col, 2)
+
+                        if winning_move(board, 2):
+                            label = myfont.render("Player 2 wins!!", 1, YELLOW)
+                            screen.blit(label, (40, 10))
+                            game_over = True
+
+                print_board(board)
+                draw_board(board)
+
+                turn += 1
+                turn = turn % 2
+
+                if game_over:
+                    pygame.time.wait(3000)
+
 
 def rules():
     while True:
@@ -195,4 +424,94 @@ def rules():
 
         pygame.display.update()
 
+def board_gen():
+    board=np.zeros((ROW_COUNT,COLUMN_COUNT))
+    return board
+
+def board_gen_gui(screen):
+    for c in range(COLUMN_COUNT):
+        for r in range(ROW_COUNT):
+            pygame.draw.circle(screen, WHITE, (int((c * height/ROW_COUNT) + 1.5*RADIUS), int((r * height/ROW_COUNT) + 1.5*RADIUS)),
+                                   RADIUS)
+
+    pygame.display.update()
+
+def main_menu():
+    while True:
+        for choice in pygame.event.get():
+            if choice.type == pygame.QUIT:
+                pygame.quit()
+
+            if choice.type == pygame.MOUSEBUTTONDOWN:
+                if (button_x - (button_width / 2)) <= mouse[0] <= (button_x + (button_width / 2)) \
+                        and (single_y - (button_height / 2)) <= mouse[1] <= (single_y + (button_height / 2)):
+                    single()
+                elif (button_x - (button_width/2)) <= mouse[0] <= (button_x + (button_width/2)) \
+                             and (multi_y - (button_height/2)) <= mouse[1] <= (multi_y + (button_height/2)):
+                    multi()
+                elif (button_x - (button_width/2)) <= mouse[0] <= (button_x + (button_width/2)) \
+                             and (rules_y - (button_height/2)) <= mouse[1] <= (rules_y + (button_height/2)):
+                    rules()
+                elif (button_x - (quit_width/2)) <= mouse[0] <= (button_x + (quit_width/2)) \
+                             and (quit_y - (quit_height/2)) <= mouse[1] <= (quit_y + (quit_height/2)):
+                    pygame.quit()
+
+        screen.blit(BG, (0, 0))
+        mouse = pygame.mouse.get_pos()
+
+        #Button size
+        button_width = width/5
+        button_height = height/15
+        button_x = width/2
+
+        #buttons
+        #single player button
+        single_y = height/2.7
+
+        single_button = Button(button_x, single_y, button_width, button_height)
+
+        if (button_x - (button_width/2)) <= mouse[0] <= (button_x + (button_width/2)) \
+                and (single_y - (button_height/2)) <= mouse[1] <= (single_y + (button_height/2)):
+            single_button.draw(screen, DARK_WHITE, 0, 0, "monospace",40, WHITE, 'One-Player')
+        single_button.draw(screen, WHITE, 1, 0, "monospace",40, WHITE, 'One-Player')
+
+        #two player button
+        multi_y = height / 2.2
+
+        multi_button = Button(button_x, multi_y, button_width, button_height)
+
+        if (button_x - (button_width/2)) <= mouse[0] <= (button_x + (button_width/2)) \
+                and (multi_y - (button_height/2)) <= mouse[1] <= (multi_y + (button_height/2)):
+            multi_button.draw(screen, DARK_WHITE, 0, 0, "monospace", 40, WHITE, 'Two-Player')
+        multi_button.draw(screen, WHITE, 1, 0, "monospace", 40, WHITE, 'Two-Player')
+
+        #rules button
+        rules_y = height/1.85
+
+        rules_button = Button(button_x, rules_y, button_width, button_height)
+
+        if (button_x - (button_width/2)) <= mouse[0] <= (button_x + (button_width/2)) \
+                and (rules_y - (button_height/2)) <= mouse[1] <= (rules_y + (button_height/2)):
+            rules_button.draw(screen, DARK_WHITE, 0, 0, "monospace",40, WHITE, 'Rules')
+        rules_button.draw(screen, WHITE, 1, 0, "monospace",40, WHITE, 'Rules')
+
+        #quit button
+        quit_y = height/1.55
+        quit_width = width/15
+        quit_height = height/18
+
+        quit_button = Button(button_x, quit_y, quit_width, quit_height)
+
+        quit_button.draw(screen, LIGHT_WHITE, 0, 0, "monospace",40, BLACK, 'Quit')
+        if (button_x - (quit_width/2)) <= mouse[0] <= (button_x + (quit_width/2)) \
+                and (quit_y - (quit_height/2)) <= mouse[1] <= (quit_y + (quit_height/2)):
+            quit_button.draw(screen, DARK_WHITE, 0, 0, "monospace", 40 ,WHITE, 'Quit')
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+        pygame.display.update()
+
 main_menu()
+
